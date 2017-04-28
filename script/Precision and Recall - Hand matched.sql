f7ac719791e160c04374c21334ef22f9a991e4b0
bardhr <bardhrexhepi@yahoo.com>
2017-04-28 16:03:55 +0200

    Evaluation for Hand matched dataset

---
+++ script/Precision and Recall - Hand matched.sql
@@ -0,0 +1,99 @@
+/*------------------ PRECISION AND RECALL FOR "TITLE FIRST AUTHOR SECOND" -----------------------------*/
+--True positives
+SELECT COUNT(*)
+FROM "CITESEER"."HAND_MATCHED_DATASET"  pm
+    INNER JOIN (
+                
+                SELECT *
+                FROM RESULT_MATCH80
+                WHERE (AUTHOR_JWS >= 75 OR AUTHOR_JWS_LEFT >=75 )
+                      AND TITLE_JWS >= 90
+                UNION
+                SELECT *
+                FROM RESULT_MATCH80
+                WHERE (AUTHOR_JWS >= 60 OR AUTHOR_JWS_LEFT >=60 )
+                      AND TITLE_JWS >= 95
+                ) presult
+              ON presult.dblp_id = pm.dblp_id and presult.citeseer_id = pm.citeseer_id
+--793
+
+
+
+
+--True positives + False positives 
+SELECT COUNT(*)
+FROM (
+  SELECT *
+  FROM RESULT_MATCH80
+  WHERE (AUTHOR_JWS >= 75 OR AUTHOR_JWS_LEFT >=75 )
+        AND TITLE_JWS >= 90
+  UNION
+  SELECT *
+  FROM RESULT_MATCH80
+  WHERE (AUTHOR_JWS >= 60 OR AUTHOR_JWS_LEFT >=60 )
+        AND TITLE_JWS >= 95
+ ) presult;
+--851
+
+--precision = 793/851 = 0.9318
+
+
+
+--True positives + False negatives 
+SELECT COUNT(*)
+FROM "CITESEER"."HAND_MATCHED_DATASET" pm
+--1038
+
+--recall = 793/1038 = 0.7640
+
+/*------------------------------------------------------------------------------------------------------*/
+
+
+/*---------------------- PRECISION AND RECALL FOR "CANOPY CLUSTERING" ------------------------------------*/
+
+select * from CANOPY_CITESEER_DBLP
+
+
+--True positives
+SELECT COUNT(*)
+FROM aercs_tt.paper_mapping pm
+    INNER JOIN aercs_tt.paper p
+              ON pm.DBLP_ID = p.ID
+    INNER JOIN paper cp
+              ON pm.CITESEER_ID = cp.id  
+    INNER JOIN CANOPY_CITESEER_DBLP canopy
+    ON canopy.dblp_id = pm.dblp_id and canopy.citeseer_id = pm.citeseer_id
+    WHERE
+      p.title LIKE 'X%' 
+      and cp.title LIKE 'X%'  
+      and (p.year <= 2010 or p.year is null)
+      and (cp.year <= 2009 or cp.year is null);
+--757
+
+--True positives + False positives 
+SELECT COUNT(*)
+FROM CANOPY_CITESEER_DBLP canopy
+inner join aercs_tt.paper dp
+on canopy.dblp_id = dp.id
+inner join citeseer.paper cp
+on canopy.citeseer_id = cp.id
+where 
+ (dp.year <= 2010 or dp.year is null)
+      and (cp.year <= 2009 or cp.year is null);
+
+--1479
+
+--precision = 757/1479 = 0.5118
+
+
+--True positives + False negatives 
+SELECT COUNT(*)
+FROM aercs_tt.paper_mapping pm
+    INNER JOIN aercs_tt.paper p
+              ON pm.DBLP_ID = p.ID
+    INNER JOIN paper cp
+              ON pm.CITESEER_ID = cp.id  
+WHERE p.title LIKE 'X%' and cp.title LIKE 'X%';
+--953
+
+--recall = 757/953 = 0.7943
